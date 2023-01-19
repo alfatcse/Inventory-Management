9184b057a749587cefba13e148ac3e1d8a347870
Alfat <alfat.cse.21@gmail.com>
2023-01-19 15:53:27 +0100

    product save to data base

--- app.js
+++ app.js
@@ -4,73 +4,92 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 app.use(express.json());
 app.use(cors());
-const productSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please Provide a Name"],
-    trim: true,
-    unique: [true, "Name must be unique"],
-    minLength: [3, "Name must be at Least 3 Characters"],
-    maxLength: [100, "Name is too large"],
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-    min: [0, "Can not be negative"],
-  },
-  unit: {
-    type: String,
-    required: true,
-    enum: {
-      values: ["kg", "pcs", "litre"],
-      message: "Unit value must be kg/pcs/litre ",
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please Provide a Name"],
+      trim: true,
+      unique: [true, "Name must be unique"],
+      minLength: [3, "Name must be at Least 3 Characters"],
+      maxLength: [100, "Name is too large"],
     },
-  },
-  quantity: {
-    type: Number,
-    required: true,
-    min: [0, "quantity can not be negative"],
-    validate: {
-      validator: (value) => {
-        const isInteger = Number.isInteger(value);
-        if (isInteger) {
-          return true;
-        } else {
-          return false;
-        }
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: [0, "Can not be negative"],
+    },
+    unit: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["kg", "pcs", "litre"],
+        message: "Unit value must be kg/pcs/litre ",
       },
     },
-    message: "Quantity must be an integer",
-  },
-  status: {
-    type: String,
-    required:true,
-    enum: {
-      values: ["in-stock", "out-of-stock", "discontinued"],
-      message:"status can not be other"
+    quantity: {
+      type: Number,
+      required: true,
+      min: [0, "quantity can not be negative"],
+      validate: {
+        validator: (value) => {
+          const isInteger = Number.isInteger(value);
+          if (isInteger) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+      message: "Quantity must be an integer",
     },
-  },
-  supplier:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"Supplier"
-  },
-  categories:[{
-    name:{
-      type:String,
-      required:true
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["in-stock", "out-of-stock", "discontinued"],
+        message: "status can not be other",
+      },
     },
-    _id:mongoose.Schema.Types.ObjectId
-  }]
-},{
-  timestamps:true
-});
-
+    // supplier:{
+    //   type:mongoose.Schema.Types.ObjectId,
+    //   ref:"Supplier"
+    // },
+    // categories:[{
+    //   name:{
+    //     type:String,
+    //     required:true
+    //   },
+    //   _id:mongoose.Schema.Types.ObjectId
+    // }]
+  },
+  {
+    timestamps: true,
+  }
+);
+const Product = mongoose.model("Product", productSchema);
 app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
 });
-
+app.post("/api/v1/product", async (req, res, next) => {
+  try {
+    const product = new Product(req.body);
+    const result = await product.save();
+    res.status(200).json({
+      status: "success",
+      message: "Data inserted",
+      data: result,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: "Data not inserted",
+      error: err.message,
+    });
+  }
+});
 module.exports = app;
