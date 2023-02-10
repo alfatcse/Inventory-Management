a16c6d60933353e5159014d6a9fdba1f8fb41ed9
Alfat <alfat.cse.21@gmail.com>
2023-02-10 20:40:58 +0100

    bulk delete done

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,7 +2,10 @@ const Product = require("../model/Product");
 const {
   getProductsService,
   postProductsService,
-  updateProductService
+  updateProductService,
+  bulkUpdateProductService,
+  deleteProductByIdService,
+  bulkDeleteProductService
 } = require("../Services/product.service");
 exports.getProducts = async (req, res, next) => {
   try {
@@ -41,17 +44,74 @@ exports.createProduct = async (req, res, next) => {
 };
 exports.updateProduct = async (req, res, next) => {
   try {
-      const {id}=req.params;
-      const productUpdate=await updateProductService(id,req.body);
-      res.status(200).json({
-        status: "success",
-        message: "Data updated",
-        data: productUpdate,
-      });
+    const { id } = req.params;
+    const productUpdate = await updateProductService(id, req.body);
+    res.status(200).json({
+      status: "success",
+      message: "Data updated",
+      data: productUpdate,
+    });
   } catch (err) {
-      res.status(400).json({
+    res.status(400).json({
       status: "fail",
       message: "Product not Updated",
+      error: err.message,
+    });
+  }
+};
+exports.bulkUpdateProduct = async (req, res, next) => {
+  try {
+    const productUpdate = await bulkUpdateProductService(req.body);
+    res.status(200).json({
+      status: "success",
+      message: "Data updated",
+      data: productUpdate,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: "Product not Updated",
+      error: err.message,
+    });
+  }
+};
+exports.deleteProductById = async (req, res, next) => {
+  try {
+    const {id}=req.params;
+    const productUpdate = await deleteProductByIdService(id);
+    res.status(200).json({
+      status: "success",
+      message: "Data deleted",
+      data: productUpdate,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: "Product not deleted",
+      error: err.message,
+    });
+  }
+};
+exports.deleteProductByIdBulk = async (req, res, next) => {
+  try {
+    const {ids}=req.body;
+    console.log(ids);
+    const productDelete = await bulkDeleteProductService(ids);
+    if(!productDelete.deletedCount){
+      return res.status(400).json({
+        status:'fail',
+        error:"Can not delete"
+      })
+    }
+    res.status(200).json({
+      status: "success",
+      message: "Data deleted",
+      data: productDelete,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: "Product not deleted",
       error: err.message,
     });
   }
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router=express.Router();
 const productController=require('../controllers/product.controller');
+
+router.route('/bulk-update').patch(productController.bulkUpdateProduct)
+router.route('/bulk-delete').delete(productController.deleteProductByIdBulk)
+
 router.route('/')
 .get(productController.getProducts)
 .post(productController.createProduct)
-router.route('/:id')
-.patch(productController.updateProduct)
+
+router.route('/:id').patch(productController.updateProduct)
+.delete(productController.deleteProductById)
+
+
 module.exports=router
